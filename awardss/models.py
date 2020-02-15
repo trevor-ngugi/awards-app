81e6c43d5ee30760bfda4f0fab4b7ce33c0d1eb4
trevor ngugi <ngugitrevor@gmail.com>
2020-02-15 20:56:24 +0300

    add user model

--- awardss/models.py
+++ awardss/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length =30)
+    bio = models.CharField(max_length =30)
+    no_of_projects = models.IntegerField(default=0)
+    #profileImage
+    contact=models.CharField(max_length =30)
+
+class ratings(models.Model):
+    design
+    usability
+    content
